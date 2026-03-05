480a534ed87407c92b42c946f0e95da1c969e777
VicMakau-78 <kyamavictor78@gmail.com>
2026-03-05 20:22:59 +0300

    modified:   src/App.css 	modified:   src/Assignment/Aboutcomponent.jsx 	modified:   src/Assignment/Contactuscomponent.jsx

--- src/Assignment/Aboutcomponent.jsx
+++ src/Assignment/Aboutcomponent.jsx
@@ -1,6 +1,6 @@
 const Aboutcomponent = () => {
     return(
-        <div>
+        <div className="Aboutcomponent">
             <h1>About Us</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, eveniet nesciunt modi quia voluptates ab eos, cupiditate corporis repellat laboriosam cum voluptate totam deleniti. Rem tempore ex quaerat quae vel beatae, quia id, repellat voluptates veniam aliquid illum ipsam obcaecati?</p>
         </div>
--- src/Assignment/Contactuscomponent.jsx
+++ src/Assignment/Contactuscomponent.jsx
@@ -1,6 +1,6 @@
 const Contactuscomponent = () => {
     return (
-        <div>
+        <div className="Contactuscomponent">
             <h1>Contact Us</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, accusantium at illum ipsum nobis, necessitatibus provident nostrum molestias ipsa atque optio. Eum, error quae atque asperiores, incidunt voluptatum ratione nesciunt quos in labore placeat iusto, esse sequi repellendus officiis odit.</p>
         </div>
